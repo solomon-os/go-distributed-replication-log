8ba54f40dcd09f19d6e53a3fedd0473d0d3b9b79
Segun Oladiran <sheghunoladiran9@gmail.com>
2026-08-12 13:01:44 +0100

    index written successfully

--- log/go.mod
+++ log/go.mod
@@ -2,7 +2,10 @@ module github.com/solomon-os/go-distributed-replication-log/log
 
 go 1.26.1
 
-require github.com/stretchr/testify v1.11.1
+require (
+	github.com/stretchr/testify v1.11.1
+	github.com/tysonmote/gommap v0.0.3
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- log/internal/log/index.go
+++ log/internal/log/index.go
@@ -1,6 +1,11 @@
 package log
 
-import "os"
+import (
+	"io"
+	"os"
+
+	"github.com/tysonmote/gommap"
+)
 
 var (
 	offWidth uint64 = 4
@@ -10,4 +15,79 @@ var (
 
 type index struct {
 	file *os.File
+	mmap gommap.MMap
+	size uint64
+}
+
+func newIndex(f *os.File, c Config) (*index, error) {
+	idx := &index{
+		file: f,
+	}
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+
+	idx.size = uint64(fi.Size())
+	if err = os.Truncate(
+		f.Name(), int64(c.Segment.MaxIndexBytes),
+	); err != nil {
+		return nil, err
+	}
+
+	if idx.mmap, err = gommap.Map(
+		idx.file.Fd(),
+		gommap.PROT_READ|gommap.PROT_WRITE,
+		gommap.MAP_SHARED,
+	); err != nil {
+		return nil, err
+	}
+
+	return idx, nil
+}
+
+func (i *index) Name() string {
+	return i.file.Name()
+}
+
+func (i *index) Read(in int64) (out uint32, pos uint64, err error) {
+	if i.size == 0 {
+		return 0, 0, io.EOF
+	}
+
+	if in == -1 {
+		out = uint32((i.size / entWidth) - 1)
+	} else {
+		out = uint32(in)
+	}
+	pos = uint64(out) * entWidth
+	if i.size < pos+entWidth {
+		return 0, 0, io.EOF
+	}
+	out = enc.Uint32(i.mmap[pos : pos+offWidth])
+	pos = enc.Uint64(i.mmap[pos+offWidth : pos+entWidth])
+	return out, pos, nil
+}
+
+func (i *index) Write(off uint32, pos uint64) error {
+	if uint64(len(i.mmap)) < i.size+entWidth {
+		return io.EOF
+	}
+	enc.PutUint32(i.mmap[i.size:i.size+offWidth], off)
+	enc.PutUint64(i.mmap[i.size+offWidth:i.size+entWidth], pos)
+	i.size += entWidth
+	return nil
+}
+
+func (idx *index) Close() error {
+	if err := idx.mmap.Sync(gommap.MS_SYNC); err != nil {
+		return err
+	}
+	if err := idx.file.Sync(); err != nil {
+		return err
+	}
+	if err := idx.file.Truncate(int64(idx.size)); err != nil {
+		return err
+	}
+	return idx.file.Close()
 }
