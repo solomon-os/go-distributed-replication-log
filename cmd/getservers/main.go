8f6f7f1d842287d05aa90c72b9e95bce74904b39
Segun Oladiran <sheghunoladiran9@gmail.com>
2026-08-25 00:49:33 +0100

    kubernetes local deployment done

---
+++ cmd/getservers/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	api "github.com/solomon-os/go-distributed-replication-log/log/api/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	addr := flag.String("addr", ":8400", "service address")
+	flag.Parse()
+	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := api.NewLogServiceClient(conn)
+	ctx := context.Background()
+	res, err := client.GetServers(ctx, &api.GetServersRequest{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("servers:")
+	for _, server := range res.Servers {
+		fmt.Printf("\t- %v\n", server)
+	}
+}
